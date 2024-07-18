1983328cb8d892d81a8ccb532d125ceae7144f76
neomond <neoweise@yahoo.com>
2024-07-18 11:39:49 +0400

    Initial Commit

---
+++ AirbnbApp/AirbnbAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  AirbnbAppApp.swift
+//  AirbnbApp
+//
+//  Created by Nazrin Atayeva on 18.07.24.
+//
+
+import SwiftUI
+
+@main
+struct AirbnbAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
