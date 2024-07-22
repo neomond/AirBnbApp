0620b2a0b260f0784f966d28e0074ff254b8d9d3
neomond <neoweise@yahoo.com>
2024-07-22 10:16:25 +0400

    feat: profile UI

---
+++ AirbnbApp/New Group/Core/Profile/ProfileOptionRowView.swift
@@ -0,0 +1,33 @@
+//
+//  ProfileOptionRowView.swift
+//  AirbnbApp
+//
+//  Created by Nazrin Atayeva on 22.07.24.
+//
+
+import SwiftUI
+
+struct ProfileOptionRowView: View {
+    let imageName: String
+    let title: String
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Image(systemName: imageName)
+                
+                Text(title)
+                    .font(.subheadline)
+                
+                Spacer()
+                
+                Image(systemName: "chevron.right")
+            }
+            Divider()
+        }
+    }
+}
+
+#Preview {
+    ProfileOptionRowView(imageName: "gear", title: "Settings")
+}
